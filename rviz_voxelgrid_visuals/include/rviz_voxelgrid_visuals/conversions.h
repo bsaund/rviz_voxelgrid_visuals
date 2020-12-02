dd7234c9cc679feaa35a7efe3dc6a4cbc81c0a00
Brad Saund <brad.saund@gmail.com>
2020-12-01 20:58:04 -0500

    Add the start of a conversions C++ library

---
+++ rviz_voxelgrid_visuals/include/rviz_voxelgrid_visuals/conversions.h
@@ -0,0 +1,16 @@
+//
+// Created by bsaund on 12/1/20.
+//
+
+#ifndef RVIZ_VOXELGRID_VISUALS_CONVERSIONS_H
+#define RVIZ_VOXELGRID_VISUALS_CONVERSIONS_H
+
+#include <std_msgs/Float32MultiArray.h>
+
+namespace RVV{
+
+std_msgs::Float32MultiArray voxelgridToFloatArray();
+
+}
+
+#endif  // RVIZ_VOXELGRID_VISUALS_CONVERSIONS_H
---
+++ rviz_voxelgrid_visuals/src/conversions.cpp
@@ -0,0 +1,10 @@
+//
+// Created by bsaund on 12/1/20.
+//
+#include "rviz_voxelgrid_visuals/conversions.h"
+#include <iostream>
+
+std_msgs::Float32MultiArray RVV::voxelgridToFloatArray() {
+  std::cout << "Called voxelgridToFloatArray()\n";
+  return std_msgs::Float32MultiArray();
+}
